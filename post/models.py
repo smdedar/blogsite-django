2dee828f8ed65c77a963de9375672157187039b1
Dedar Alam <thededar@gmail.com>
2021-02-23 03:37:35 +0600

    update

--- post/models.py
+++ post/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class post(models.Model):
+    title = models.CharField(max_length=255, default='0000000')
     text = models.TextField()
 
     def __str__(self) -> str:
